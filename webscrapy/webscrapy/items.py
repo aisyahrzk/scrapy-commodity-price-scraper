1ea800466ab573d5523a008f22c4cbaa6fc505b4
Aisyah Razak <aisyahrazak171@gmail.com>
2023-01-01 13:00:02 +0800

    add scraped data items

--- webscrapy/webscrapy/items.py
+++ webscrapy/webscrapy/items.py
@@ -6,7 +6,25 @@
 import scrapy
 
 
-class WebscrapyItem(scrapy.Item):
+class WebscrapyDailyCommodityItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    #location of [product variety price report
+    NamaPusat = scrapy.Field()
+
+    #Name of daily commodity product
+    NamaVarieti = scrapy.Field()
+
+    #Unit measurement of product
+    UnitBarang = scrapy.Field()
+
+    #Average reported price of one unit of product
+    HargaPurata = scrapy.Field()
+
+    #Highest reported price of one unit of product
+    HargaTinggi = scrapy.Field()
+
+    #Lowest reported price of one unit of product
+    HargaRendah = scrapy.Field()
+    
