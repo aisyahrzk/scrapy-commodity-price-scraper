210ae7c2f88b8724ca9b779ac5252330905965e3
Aisyah Razak <aisyahrazak171@gmail.com>
2023-01-10 23:59:40 +0800

    modify codes for scraping multiple pages

--- webscrapy/webscrapy/spiders/fama.py
+++ webscrapy/webscrapy/spiders/fama.py
@@ -3,6 +3,10 @@ from datetime import datetime
 import time
 import json
 from ..items import WebscrapyDailyCommodityItem
+from datetime import date, timedelta
+from scrapy.http import Request
+import pandas as pd
+
 
 class FamaSpider(scrapy.Spider):
     name = 'fama'
@@ -11,6 +15,16 @@ class FamaSpider(scrapy.Spider):
 
 
     def parse(self, response):
+
+        start_date = date(2015, 1, 1)
+        end_date = date(2023,1,9)
+        delta = timedelta(days=1)
+        urls = ('https://sdvi2.fama.gov.my/price/direct/price/daily_commodityRpt.asp?Pricing=A&LevelCd=03&PricingDt={}&PricingDtPrev={}'.format(i) for i in pd.date_range(start_date,end_date))
+        for url in urls:
+            yield Request(url, callback=self.parse_page)
+
+    def parse_page(self, response):
+        
 
         pusat = response.xpath("/html/body/table/tr/td")
 
@@ -26,8 +40,3 @@ class FamaSpider(scrapy.Spider):
             item["HargaPurata"] = p.xpath(".//table[position() mod 2 = 0]/tr[contains(@id,content-body)][position() > 2]/td[5]/text()").extract()
             item["HargaRendah"] = p.xpath(".//table[position() mod 2 = 0]/tr[contains(@id,content-body)][position() > 2]/td[6]/text()").extract()
             
-
-    
-             
-            #//*[@id="D101"]/td[1] /html/body/table/tbody/tr[5]/td/table[2]/tbody/tr[3]/td[1]
-            #/html/body/table/tbody/tr[5]/td/table[1]/tbody/tr/td/b
